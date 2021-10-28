fc00d47f49ee2646c67b60539338e16f48da7ed5
Tigran <tigran.contact@gmail.com>
2021-10-29 01:17:24 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import PlayIcon from './play.svg';
+import PauseIcon from './pause.svg';
+
+const audio = new Audio();
 
 function App() {
+  const [track, setTrack] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [canPlay, setCanPlay] = useState(false);
+
+  const getNextTrack = () => {
+    return fetch('http://localhost:9003/current')
+      .then((response) => response.json())
+      .then((response) => {
+        setTrack(response);
+        audio.src = response.metadata.file;
+      });
+  };
+
+  const play = () => {
+    setLoading(true);
+    setPlaying(true);
+    getNextTrack().catch(() => {
+      setLoading(false);
+      setPlaying(false);
+    });
+  };
+
+  const stop = () => {
+    audio.pause();
+    setPlaying(false);
+  };
+
+  const onPlaying = () => {
+    setLoading(false);
+    setPlaying(true);
+  };
+
+  const onCanplay = () => {
+    setCanPlay(true);
+  };
+
+  useEffect(() => {
+    if (canPlay && track) {
+      audio.currentTime = track.time;
+      audio.volume = 1;
+      audio.play();
+    }
+  }, [canPlay, track]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', play);
+    audio.addEventListener('canplay', onCanplay);
+    audio.addEventListener('playing', onPlaying);
+    return () => {
+      audio.removeEventListener('ended', play);
+      audio.removeEventListener('canplay', onCanplay);
+      audio.removeEventListener('playing', onPlaying);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {playing ? <img src={PauseIcon} onClick={stop} /> : <img src={PlayIcon} onClick={play} />}
+      <div className="info">
+        {loading && 'Loading...'}
+        {!loading && track && (
+          <>
+            {track?.metadata?.title} by <b>{track?.metadata?.artist}</b>
+          </>
+        )}
+      </div>
     </div>
   );
 }
