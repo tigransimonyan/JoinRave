6caf06a64cdfc5c1db18bba91981eddf6ed9d02f
Tigran Simonyan <tigran@frontjet.com>
2022-04-13 20:41:15 +0400

    update title

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,12 +1,13 @@
-import './style.css';
+import "./style.css";
 
 function About() {
   return (
     <div className="about">
       <h4 className="about-title">about</h4>
       <p className="about-text">
-        Joinrave.com is a community based, commercial-free, electronic music radio station. We are
-        always looking for creative people to join us to create an online radio show.
+        Joinrave.com is a community based, commercial-free, electronic music
+        online radio. We are always looking for creative people to join us to
+        create an radio show.
       </p>
     </div>
   );
